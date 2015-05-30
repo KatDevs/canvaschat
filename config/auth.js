4ffac8b6916bed462c2259db48394ef0fb207dbf
Nikhilesh Katakam <NKatakam@walmart.com>
2015-05-30 16:18:48 -0700

    adding facebook linking feature

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 	development:{
 	    'facebook' : {
-	        'consumerKey': '',
-	        'consumerSecret': '',
+	        'consumerKey': '846767545359932',
+	        'consumerSecret': 'ff7e28149a5dcde3ca0b6dc4fa40ae47',
 	        'callbackUrl': 'http://socialauthenticator.com:8000/auth/facebook/callback'	        					   
 	    }		
 	}
